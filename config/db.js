449b705f28c8e2d5c2c39ea56218f95f91862ef4
rogong <itingasng@gmail.com>
2022-03-08 12:22:53 +0100

    add mongo options

--- config/db.js
+++ config/db.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
-//const config = require('config');
-//const db = config.get('mongoURL');
+
+var options = {
+  "mongos": {
+    "ssl": true,
+    "sslValidate": false
+  }
+};
 
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI, options);
     
     console.log('MongoDB Connected...');
   } catch (err) {
